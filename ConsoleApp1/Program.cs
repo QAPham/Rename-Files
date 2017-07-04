ca65c5c522138874d5f8f53f3902f592c028e4f2
Kiana Pham <KianaP@gallagher.local>
2017-07-05 11:43:37 +1200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"U:\\transit\\Kiana\\Image4");
+            FileInfo[] f = d.GetFiles().ToArray();
+            foreach (FileInfo file  in f)
+            {
+                string[] s = file.FullName.Split(' ');
+                int temp = s.Length;
+                if(temp != 1)
+                {
+                    string newName = "";
+                    for (int i = 0; i < temp ; i++)
+                    {
+                        if(i != temp - 1)
+                        {
+                            newName += s[i] + "_";
+                        }
+                        else
+                        {
+                            newName += s[i];
+                        }
+                        
+                    }
+                    File.Move(file.FullName, newName);
+                }
+            }
+        }
+    }
+}
